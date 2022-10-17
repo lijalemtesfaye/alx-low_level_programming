615463c29dc0d4db7040f20b166aba0cc18e7d9c
lijalemtesfaye <lijalemtesfaye05@gmail.com>
2022-10-17 14:55:00 +0300

    Hello Printf Task commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -entry point
+ *
+ * Return: always 0 (Sucess)
+ */
+int main(void){
+	printf("with proper grammer ,but the outcome is a piece of art, \n")
+		return (0);
+}
