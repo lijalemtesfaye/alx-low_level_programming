5ef0115f46c9236ad66330a1d3303034fdeaf25e
lijalemtesfaye <lijalemtesfaye05@gmail.com>
2022-10-17 15:25:16 +0300

    Print alphabet in Lowercase Task commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: Get a random number and check its last digit, compare it with 5
+ * Return: 0
+ */
+int main(void)
+{
+	char m = 'a';
+
+	while (m <= 'z')
+	{
+		putchar(m);
+		m++;
+	}
+
+	putchar('\n');
+	return (0);
+}
