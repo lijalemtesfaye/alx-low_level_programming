5013c0c5fbeff87945239c75176ee4811d4861c7
lijalemtesfaye <lijalemtesfaye05@gmail.com>
2022-10-17 16:04:55 +0300

    Possible commbination of all two digit yask commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,6 +14,7 @@
  * You can only use putchar five times maximum in your code
  * You are not allowed to use any variable of type char
  * All your code should be in the main function
+ * return 0
  */
 int main(void)
 {
