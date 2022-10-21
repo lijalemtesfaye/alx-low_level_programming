0e7c2ef236b379dfacd098f4cb8158482aeb5f64
lijalemtesfaye <lijalemtesfaye05@gmail.com>
2022-10-21 12:16:13 +0300

    Task 3 commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,15 @@
 #include "main.h"
 /**
- * print_numbers - print numbers
- * Return: Always 0.
+ * print_numbers - prints the numbers, from 0 to 9, followed by a new line
+ * Return - void
  */
 void print_numbers(void)
 {
-int i;
+	int i;
 
-i = 0;
-while (i < 10)
-{
-_putchar (i + '0');
-i++;
-}
-
-_putchar ('\n');
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,7 @@
+#ifndef MAIN
+#define MAIN
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
+extern void print_numbers(void);
+#endif
