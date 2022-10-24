a2de3cf349cedf2e58b33977b48366c0a6cb0090
lijalemtesfaye <lijalemtesfaye05@gmail.com>
2022-10-24 13:35:18 -0700

    Task 0 commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-
-
 /**
  *
  *  * reset_to_98 - updates the value *n points to.
@@ -9,13 +7,8 @@
  *   * @n: the number to modify.
  *
  *    */
-
-
-
 void reset_to_98(int *n)
 
 {
-
 		*n = 98;
-
 }
