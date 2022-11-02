64695ea971f0a9c47473ff866739ba3c0ddaa627
lijalemtesfaye <lijalemtesfaye05@gmail.com>
2022-11-02 13:54:46 -0700

    Task 0 commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,13 +10,13 @@
  *     */
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s)
 	{
 		_putchar('\n');
+		_put_recursion(s + 1);
 	}
 	else
 	{
-	_putchar('\n');
-	_put_recursion(s + 1);
+		_putchar('\n');
 	}
 }
