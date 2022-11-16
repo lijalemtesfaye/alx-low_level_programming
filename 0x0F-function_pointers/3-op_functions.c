6b7c8ab2656548254709aff69e6a53edb8fb3591
lijalemtesfaye <lijalemtesfaye05@gmail.com>
2022-11-16 15:20:43 +0300

    All Task are Commited here

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,50 +1,62 @@
-#include "3-calc.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
 
 /**
- * op_add - ...
- * @a: ...
- * @b: ...
+ * op_add - Adds two numbers @a and @b
  *
- * Return: ...
+ * @a: A number
+ *
+ * @b: A number
+ *
+ * Return: Sum of @a and @b
  */
+
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
 /**
- * op_sub - ...
- * @a: ...
- * @b: ...
+ * op_sub - Calculates the difference of @c and @b
  *
- * Return: ...
+ * @a: A number
+ *
+ * @b: A number
+ *
+ * Return: Difference of @a and @b
  */
+
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
 /**
- * op_mul - ...
- * @a: ...
- * @b: ...
+ * op_mul - Multiplies two numbers
  *
- *  Return: ...
+ * @a: A number
+ *
+ * @b: A number
+ *
+ * Return: Product of @a and @b
  */
+
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
 /**
- * op_div - ...
- * @a: ...
- * @b: ...
+ * op_div - Divides @a by @b
  *
- *  Return: ...
+ * @a: A number
+ *
+ * @b: A number
+ *
+ * Return: Result of the division of @a by @b without remainder
  */
+
 int op_div(int a, int b)
 {
 	if (b == 0)
@@ -57,12 +69,15 @@ int op_div(int a, int b)
 }
 
 /**
- * op_mod - ...
- * @a: ...
- * @b: ...
+ * op_mod - Calculates the remainder of @a divided by @b
  *
- * Return: ...
+ * @a: A number
+ *
+ * @b: A number
+ *
+ * Return: Remainder of @a divider by @b
  */
+
 int op_mod(int a, int b)
 {
 	if (b == 0)
